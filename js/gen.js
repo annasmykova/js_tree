ad0479413a206ed1ff4410e1ab1677c6c8f35368
sheroshtan <sheroshtan1@gmail.com>
2017-12-19 19:21:41 +0300

    create object for family tree

--- js/gen.js
+++ js/gen.js
@@ -1,1 +1,37 @@
-//write here :)
+let rayanGosling = {
+  name: 'Rayan',
+  photo: 'https://lover.ru/cache/images/9c4d96175c1835df4854e91008218203/resizeCrop_700_525_center_center__.jpg',
+  mother: { // mama
+    name: 'Jessica',
+    photo: 'https://vignette.wikia.nocookie.net/vegascbs/images/b/bc/Carrie-Anne_Moss.jpg/revision/latest?cb=20120917012237',
+    mother: { //babushka
+      name: 'Maryl',
+      photo: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTE5NTU2MzE2MTI4MjQ5MzU1/jane-lynch-585264-1-402.jpg',
+      mother: null,
+      father: null
+    },
+    father: { //ded
+      name: 'Shown',
+      photo: 'http://cdn.smosh.com/sites/default/files/2015/01/old-actors-connery1.jpg',
+      mother: null,
+      father: null
+    }
+  },
+  father: { // papa
+    name: 'Alec',
+    photo: 'https://www.thewrap.com/wp-content/uploads/2014/06/Alec-Baldwin.jpg',
+    mother: { //babushka
+      name: 'Joanne',
+      photo: 'http://i.telegraph.co.uk/multimedia/archive/01913/jk-rowling_1913245c.jpg',
+      mother: null,
+      father: null
+
+    },
+    father: { //ded
+      name: 'Bill',
+      photo: 'http://cdn1-www.craveonline.com/assets/mandatory/legacy/2013/04/man_file_1043888_woody-allen-funniest-actors-300x300.jpg',
+      mother: null,
+      father: null
+    }
+  }
+};
